d77c6671f02237e7bd09798a5f69153f9abfaa84
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-08-09 17:32:26 -0300

    Initial Commit

---
+++ app/src/main/java/com/meuprojeto/sorteador/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.meuprojeto.sorteador
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val numeroExibido = findViewById<TextView>(R.id.text)
+        val botao = findViewById<Button>(R.id.button)
+
+        botao.setOnClickListener {
+            val numeroAleatorio = Random.nextInt(101)
+            numeroExibido.text = numeroAleatorio.toString()
+        }
+
+
+    }
+}
